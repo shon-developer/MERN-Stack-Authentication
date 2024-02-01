a8190980a969700141662d506b8851efac7bae00
shon <shonbwd@gmail.com>
2024-02-01 13:33:30 +0530

    userSchema

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import JWT from "jsonwebtoken";
+import config from "../config/index.js";
+import AuthRoles from "../utils/AuthRoles.js";
 
 const userSchema = new mongoose.Schema(
   {
@@ -26,8 +30,33 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
+    },
   },
   { timestamps: true }
 );
 
-export default mongoose.Model("User", userSchema);
+// encrypt password before saving || hooks
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods = {
+  // compare passwords
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+  // create jwt token
+  getJWTtoken: function () {
+    return JWT.sign({ _id: this._id }, config.JWT_SECRET, {
+      expiresIn: config.JWT_EXPIRY,
+    });
+  },
+};
+
+export default mongoose.model("User", userSchema);
