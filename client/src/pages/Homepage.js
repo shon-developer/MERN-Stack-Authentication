af17d3b441e6f9212c528ad0629715bbc14097a8
Shon L <119747143+shon-developer@users.noreply.github.com>
2024-02-06 14:36:54 +0530

    Update Homepage.js

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -9,7 +9,7 @@ const Homepage = () => {
       <div className="w-full h-full">
         <div className="max-w-7xl mx-auto">
           <h1 className="w-full text-center py-10 text-4xl text-cyan-500">
-            Name
+            Home
           </h1>
           <pre>{JSON.stringify(auth, null, 4)}</pre>
         </div>
