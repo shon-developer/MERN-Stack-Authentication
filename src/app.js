c3c25e30e0388aa04ee12278d040fe34fca224c4
shon <shonbwd@gmail.com>
2024-02-06 13:41:57 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import crypto from "crypto";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// es module fix
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // rest object
 const app = express();
@@ -15,6 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -25,8 +32,8 @@ let key = crypto.randomBytes(64).toString("hex");
 console.log(key);
 */
 // rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Authentication</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 export default app;
